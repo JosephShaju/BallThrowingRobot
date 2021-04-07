731a5e536f311d69254014d94f14a9cafbcffd26
Joseph Shaju <josephcoolbro1@gmail.com>
2021-04-06 20:35:59 -0400

    Update GraspBall.py

--- GraspBall.py
+++ GraspBall.py
@@ -64,32 +64,42 @@ if __name__ == '__main__':
     q50 = currentJointState.position[4]
     q60 = currentJointState.position[5]
     #1.6962834294765.   1.66283127117. 1.67592124617
-    graspBall1 = [-1.7239178628693832, 1.6962834294765, 3.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
-    graspBall2 = [-1.7239178628693832, 1.67592124617, 3.39262056572, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
-    graspBall3 = [-1.7239178628693832, 1.66283127117, 3.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
-    graspBall4 = [-1.7239178628693832, 1.676648467, 3.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
+    #graspBall1 = [-1.7239178628693832, 1.6962834294765, 3.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
+    graspBallLeft = [-3.29471786287, 1.67592124617, 3.39262056572, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
+    graspBallRight = [-0.15311786286, 1.676648467, 3.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
+    graspBallLeft2 = [-3.29471786287, 1.67592124617, 3.39262056572, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
+    
+    #8.309801772021785e-05, 2.9006601314232663, 1.2986910653187502, -2.07982669269741, 1.401891180285613, -1.8971810852086435e-05, 1.000071137932979, 1.0011800646481124, 0.9996488391669853, 0.00953578528302046, 0.0018044499440588169, 0.001533493570360811
+    #graspBallRight2 = [-0.15311786286, 1.676648467, 3.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
+    graspBallNormal = [-1.7239178628693832, 1.66283127117, 3.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
     throwPosn = [-1.7239178628693832, 2.5543, 4.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
-   
-    q11 = graspBall1[0]
-    q21 = graspBall1[1]
-    q31 = graspBall1[2]
-    q41 = graspBall1[3]
-    q51 = graspBall1[4]
-    q61 = graspBall1[5]
+    
+    keepPosnRight= [-0.15311786286, 2.5543, 4.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
+    keepPosnLeft= [-0.15311786286, 2.5543, 4.3961112257191512, -3.38462117823865, 1.4459102321300366, 0.20018753676786538]
 
-    q12 = graspBall2[0]
-    q22 = graspBall2[1]
-    q32 = graspBall2[2]
-    q42 = graspBall2[3]
-    q52 = graspBall2[4]
-    q62 = graspBall2[5]
+    qleft1 = graspBallLeft[0]
+    qleft2 = graspBallLeft[1]
+    qleft3 = graspBallLeft[2]
+    qleft4 = graspBallLeft[3]
+    qleft5 = graspBallLeft[4]
+    qleft6 = graspBallLeft[5]
 
-    q13 = graspBall3[0]
-    q23 = graspBall3[1]
-    q33 = graspBall3[2]
-    q43 = graspBall3[3]
-    q53 = graspBall3[4]
-    q63 = graspBall3[5]
+    # -0.25 in x and +0.75 in z 0.375 in y top ledge
+    # +0.75 in x, +0.5 in z and 0.375 in y bottom ledge
+
+    qright1 = graspBallRight[0]
+    qright2 = graspBallRight[1]
+    qright3 = graspBallRight[2]
+    qright4 = graspBallRight[3]
+    qright5 = graspBallRight[4]
+    qright6 = graspBallRight[5]
+
+    q13 = graspBallNormal[0]
+    q23 = graspBallNormal[1]
+    q33 = graspBallNormal[2]
+    q43 = graspBallNormal[3]
+    q53 = graspBallNormal[4]
+    q63 = graspBallNormal[5]
 
     t1 = throwPosn[0]
     t2 = throwPosn[1]
@@ -98,7 +108,7 @@ if __name__ == '__main__':
     t5 = throwPosn[4]
     t6 = throwPosn[5]
 
-    gripperGrasp = [0.7, 0.6, 0.5]
+    # gripperGrasp = [0.7, 0.6, 0.5]
 
 
     def origPosition():
@@ -115,6 +125,12 @@ if __name__ == '__main__':
     
     def aboveBallPosition1():
         moveJoint([q13,q23,q33,q43,q53,q63])
+    
+    def aboveLeftBallPosition():
+        moveJoint([qleft1,qleft2,qleft3,qleft4,qleft5,qleft6])
+    
+    def aboveRightBallPosition():
+        moveJoint([qright1,qright2,qright3,qright4,qright5,qright6])
 
     def gripBall1():
         grip = [1.055, 1.065, 1.065]
@@ -135,8 +151,10 @@ if __name__ == '__main__':
     while (i == "m"):
         print("Which position?")
         b = raw_input()
-        if b == "1":
-            aboveBallPosition1()
+        if b == "l":
+            aboveLeftBallPosition()
+        if b == "r":
+            aboveRightBallPosition()        
         print("Move?")
         i = raw_input()
     
@@ -161,13 +179,10 @@ if __name__ == '__main__':
     '''
     # Force control ??
     # Final state
-
     xf = 0.0
     yf = 0.75
     zf = 0.5
     ###########################################################################
-
-
     # Boundary conditions (DON'T WORRY ABOUT THESE, and DON'T UNCOMMENT)
     init_q  = Matrix([[-q10], [q20-pi/2], [q30+pi/2], [q40], [q50-pi], [q60+pi/2]])
     #init_q1  = Matrix([[-q11], [q21-pi/2], [q31+pi/2], [q41], [q51-pi], [q61+pi/2]])
@@ -191,7 +206,6 @@ if __name__ == '__main__':
     X = []
     Y = []
     Z = []
-
     # Matrix calculatations
     dynMatrix,_ = systemKinematics(init_q)
     row0 = dynMatrix.row(0)
@@ -199,7 +213,6 @@ if __name__ == '__main__':
     row2 = dynMatrix.row(2)
     DynMatrix = Matrix([[row0],[row1],[row2]])
     J = robotJacobian(DynMatrix)
-
     # Connect all points in mesh path (START LOOP AT 1, NOT 0)
     for i in range(1,numPts):
       path_X = Matrix([[xPath[i]],[yPath[i]],[zPath[i]]])
@@ -213,14 +226,11 @@ if __name__ == '__main__':
         delta_X = path_X - Matrix([[init_X[0]],[init_X[1]],[init_X[2]]])
         print(Abs(delta_X.norm()))
         count +=1
-
         # Linearize dynamics matrix
         useful_J = evalJacobian(J,init_q)
-
         # Calculate joint state change and new state
         delta_q = inverseKinematics(useful_J,step,delta_X)
         init_q = init_q + step*delta_q
-
         # Convert back from DH angles to JACO angles
         moveFingers([1.05, 1.08, 1.08])
         moveJoint([-init_q[0], init_q[1]+pi/2, init_q[2]-pi/2, init_q[3], init_q[4]+pi, init_q[5]-pi/2])
@@ -231,10 +241,8 @@ if __name__ == '__main__':
         Z.append(init_X[2])
  
     #moveFingers([0.9,0.9,0.9])
-
     elapsed = time.time() - ti
     print(elapsed)
-
     # fig = plt.figure()
     # ax = fig.gca(projection='3d')    
     # ax.plot(X, Y, Z, label='traveled')
